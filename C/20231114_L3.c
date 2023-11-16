5998f0dceb171f6dff98cecdef4b911e578c69bb
WuXuan <81794985+WuXuan9767@users.noreply.github.com>
2023-11-16 13:18:14 +0800

    Update 20231114_L3.c

--- C/20231114_L3.c
+++ C/20231114_L3.c
@@ -12,6 +12,15 @@ int main()
     {
     printf("%c",*ch);
     }
-    
+    char *ch[100];
+    printf("请输入一串字符\n");
+    for (int a=0; a<6; a++)
+    {
+        scanf("%c",&ch[a]);
+    }
+    for(int i=0;i<6;i++)
+    {
+    printf("%c",*ch);
+    }
 
 }
