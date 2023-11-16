32e311c45dec30dfc29d7f03b2a3c68932e530a1
WuXuan <B2868939778@Gmail.com>
2023-11-16 13:14:58 +0800

    1

--- C/20231114_L3.c
+++ C/20231114_L3.c
@@ -2,10 +2,16 @@
 
 int main()
 {
-    char ch[100];
-    printf("请输入一串字符");
-    gets(ch);
-
+    char *ch[100];
+    printf("请输入一串字符\n");
+    for (int a=0; a<6; a++)
+    {
+        scanf("%c",&ch[a]);
+    }
+    for(int i=0;i<6;i++)
+    {
+    printf("%c",*ch);
+    }
     
 
 }
