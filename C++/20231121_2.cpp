4def45114882e524ada0ad2a2f0955f8a04a69a3
WuXuan <B2868939778@Gmail.com>
2023-11-21 21:13:24 +0800

    11-21

---
+++ C++/20231121_2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int n=1;
+
+void func(){
+    static int a;
+    int b=5;
+    a+=2;
+    b+=5;
+    n+=12;
+    cout << "a="<<a;
+    cout << " b="<<b;
+    cout << " n="<<n<<endl;
+}
+
+int main(){
+    static int a;
+    int b=-10;
+    cout << "a="<<a;
+    cout << " b="<<b;
+    cout << " n="<<n<<endl;//a=0 b=-10 n=1
+    func();//a=2 b=10 n=13
+    cout << "a="<<a;
+    cout << " b="<<b;
+    cout << " n="<<n<<endl;//a=0 b=-10 n=13
+    func();//a=4 b=10 n=25
+    int m;
+    cout <<m;
+}
