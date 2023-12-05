8d9a1417d37213d46d4d115d4cdd33222dd16ec6
WuXuan <2868939778@qq.com>
2023-12-05 16:27:28 +0800

    17

--- C++/20231204.cpp
+++ C++/20231204.cpp
@@ -1,3 +1,8 @@
 #include<iostream>
 using namespace std;
-
+int main(){
+    static int a[]={1,2,4,8,16};
+    cout <<"内存大小"<<sizeof(a)<<endl;
+    cout <<"机器字长"<<sizeof(int)<<endl;
+    cout <<"元素个数"<<sizeof(a)/sizeof(int)<<endl;    
+}
