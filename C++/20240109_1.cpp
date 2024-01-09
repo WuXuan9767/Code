fe774eff7a3aee439d0a0b32d6849d2fa2715baf
WuXuan <B2868939778@Gmail.com>
2024-01-09 21:05:07 +0800

    5644

---
+++ C++/20240109_1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void strCopy(char dest[], char source[]){
+        int i=0;
+    while(source[i] != 0){
+            dest[i]=source[i];
+            i++;
+        }
+        dest[i] = '\0';
+}
+
+int main(){
+    char a[20]={"asdfsfgs"};
+    char b[20];
+    strCopy(b,a);
+    cout <<b<<endl<<a<<endl;
+}
