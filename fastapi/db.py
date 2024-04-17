888e5d7e98c9fede1d3710b1ac86f9c3815cce18
WuXuan <B2868939778@Gmail.com>
2024-04-17 21:12:55 +0800

    1

--- fastapi/db.py
+++ fastapi/db.py
@@ -2,8 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://user:password@localhost/fastapi"
+
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:159847263ZFR.zfr@localhost/fastapi"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+session = SessionLocal()
